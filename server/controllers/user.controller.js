3effa01d07a219ef14143c6e2e08cfd9b18b6307
tamanna1809 <tamanna.23bsc10133@sst.scaler.com>
2024-11-02 21:19:49 +0530

    create a test api route

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,4 @@
+export const test = (req, res) => {
+  //makig test api route, getting information so get
+  res.send("API route is working");
+};
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import { type } from "os";
+
 
 //create user schema and set rules
 const userSchema= new mongoose.Schema({
