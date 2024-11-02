f9a5c22df0445c13a0bff3d3377171241b023043
tamanna1809 <tamanna.23bsc10133@sst.scaler.com>
2024-11-02 23:47:33 +0530

    create signup api route

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+  const hashPassword = bcryptjs.hashSync(password, 10); //password should be hidden in databse too
+  const newUser = new User({ username, email, password: hashPassword }); // jo model bana h User usse database main store kr rhe
+
+  try {
+    await newUser.save(); // saves in data base, await-> jab tak ye line completely run nhi hogi it won't go further
+    res.status(201).json("user created successfully");
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -3,6 +3,6 @@ import { test } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.get("/test", test);
+router.get("/test", test); //test function is called from controller 
 
 export default router;
